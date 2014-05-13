b0d527706f94430ced1dc4c9de74831f16999bbd
Mauro Agnoletti <mauro.agnoletti@gmail.com>
2014-05-13 11:06:31 -0300

    Web Socket Client added

---
+++ Client/Windows8/MessageReceivedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GuessMyNumber.Client.Windows8
+{
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+
+        public MessageReceivedEventArgs(string message)
+        {
+            this.Message = message;
+        }
+    }
+}
